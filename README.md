e9287c80cb418753e9d8f390ffa56561c55247de
ma-mrl <marion.mrl@live.fr>
2024-07-08 14:56:50 +0100

    finalised project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quote Api
+
+## Codecademy Project
+
+### Overview
+
+This project is slightly different than others you have encountered thus far on Codecademy. Instead of a step-by-step tutorial, this project contains a series of open-ended requirements which describe the project you’ll be building. There are many possible ways to correctly fulfill all of these requirements, and you should expect to use the internet, Codecademy, and other resources when you encounter a problem that you cannot easily solve.
+
+### Project Goals
+
+In this project, you’ll be building a small Express.js web API to store and serve different quotes about computers, coding, and technology.
--- server.js
+++ server.js
@@ -8,3 +8,42 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+// GET /api/quotes/random route
+app.get('/api/quotes/random', (req, res) => {
+    res.send({ 
+        quote : getRandomElement(quotes) 
+    })  
+})
+
+// GET /api/quote route
+app.get('/api/quotes', (req, res) => {
+    if (req.query.person !== undefined) {
+        const quotesByPerson = quotes.filter(quote => quote.person === req.query.person);
+        res.send({ 
+            quotes: quotesByPerson 
+        });
+    } else {
+        res.send({ 
+            quotes: quotes 
+        });
+    }
+})
+
+// POST /api/quotes
+app.post('/api/quotes', (req, res) => {
+    const newQuote = {
+        quote: req.query.quote,
+        person: req.query.person
+    };
+    if (newQuote.quote && newQuote.person) {
+        quotes.push(newQuote);
+        res.send({ quote: newQuote });
+    } else {
+        res.status(400).send();
+    }
+})
+
+// Set the server to listen on the PORT variable
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+})
